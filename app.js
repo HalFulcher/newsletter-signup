bcd68ce6b8962d2bcf7ccd213f73d30ce02a1c15
Hal Fulcher <hallam.fulcher@gmail.com>
2021-01-21 17:05:31 +0000

    App ready for Heroku deployment

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
-//jshint esversiono: 6
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/signup.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post('/', function(req, res){
-    const firstName = req.body.fName;
-    const lastName = req.body.lName;
-    const email = req.body.email;
-
-    console.log(firstName, lastName, email);
+client.setConfig({
+  apiKey: "df9a9e161cc00e2f58c66b5338dc147f-us7",
+  server: "us7",
 });
 
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
 
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("63d70bd98c", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
+        },
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
 });
 
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
 
-
-
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000.");
+});
